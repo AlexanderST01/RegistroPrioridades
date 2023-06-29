1aee17c9ad22db9395252c2d4787d76c1f392ed1
AlexanderST01 <alexandersuareztaveras@gmail.com>
2023-06-28 23:03:16 -0400

    Creando modelo

---
+++ Models/Prioridades.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Prioridades
+{
+    [Key]
+    public int PrioridadId { get; set; }   
+    [Required(ErrorMessage ="La descripcion es obligatoria")]
+    public string? Descripcion{ get; set; }
+    [Range(1, 31, ErrorMessage = "Los dias compromiso estan entre 1 y 31")]
+    public int DiasCompromiso{ get; set; }
+    
+
+}
